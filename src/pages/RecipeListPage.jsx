3b8aba6382cd45ec20ce5b6746f3e1e59bfc0cfe
TaraCharlotte <49911783+TaraCharlotte@users.noreply.github.com>
2023-09-08 21:34:28 +0200

    Update RecipeListPage.jsx

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -2,26 +2,20 @@ import { Heading } from "@chakra-ui/react";
 import { data } from "../utils/data";
 import { Searchbar } from "../components/Searchbar";
 import { useState } from "react";
-//import { RecipeList } from "../components/RecipeList";
 
 export const RecipeListPage = ({ clickFn }) => {
   const [searchField, setSearchField] = useState("");
-
-  // console.log(data.hits[1].recipe.label);
 
   const recipesSimple = [];
   for (const items in data.hits) {
     recipesSimple.push(data.hits[items].recipe);
   }
 
-  // console.log("recipes simple searchbar");
-  // console.log(recipesSimple);
 
   const matchedRecipes = recipesSimple.filter((recipe) => {
     return recipe.label.toLowerCase().includes(searchField.toLowerCase());
   });
 
-  // console.log(matchedRecipes);
 
   return (
     <div className="RecipeListPage">
